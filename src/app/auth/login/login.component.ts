2671626569ba569dcd1ab6abfe819534d8dc4322
Gabriel Borsero <gabriel.borsero@gmail.com>
2020-04-05 23:26:47 -0300

    Beginning of Section 2

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import {Store} from "@ngrx/store";
+import {Store} from '@ngrx/store';
 
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import {AuthService} from '../auth.service';
+import {tap} from 'rxjs/operators';
+import {noop} from 'rxjs';
+import {Router} from '@angular/router';
+import {AppState} from '../../reducers';
 
 @Component({
   selector: 'login',
@@ -18,9 +19,11 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-      private fb:FormBuilder,
+      private fb: FormBuilder,
       private auth: AuthService,
-      private router:Router) {
+      private router: Router,
+      private store: Store<AppState>
+      ) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
@@ -34,6 +37,24 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    const formValue = this.form.value;
+
+    this.auth.login(formValue.email, formValue.password)
+      .pipe(
+        tap((user) => {
+          console.log(user);
+
+          // dispatching an action to the store is a side-effect;
+          // this.store.dispatch
+
+          this.router.navigate(['/courses']);
+        })
+      )
+      .subscribe(
+        noop,
+        () => alert('Login Failed')
+      );
+
 
   }
 
---
+++ src/app/reducers/index.ts
@@ -0,0 +1,20 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+
+// Global state saved inside the store
+export interface AppState {
+
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+
+};
+
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
